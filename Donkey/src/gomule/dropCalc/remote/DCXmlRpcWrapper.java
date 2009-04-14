d005869a7b34da4a1e2bfda73399ad1acefd395f
silospen <>
2009-04-14 13:21:21 +0000

    Adding more methods to transfer data

--- Donkey/src/gomule/dropCalc/remote/DCXmlRpcWrapper.java
+++ Donkey/src/gomule/dropCalc/remote/DCXmlRpcWrapper.java
@@ -39,6 +39,8 @@ public class DCXmlRpcWrapper {
 	
 	public Object[] performLookup(int nType, int monKey, int mIndex, int nPlayers, int nGroup, boolean sevenPicks){
 		
+//		getMonsters(0);
+		
 		switch(monKey){
 		case 0:
 			
@@ -52,20 +54,20 @@ public class DCXmlRpcWrapper {
 		
 	}
 	
-	public Object[] getMonsters(int monKey){
-		switch(monKey){
-		case 0:
-			return extractName(DCXmlRpcServer.DC.getMainRegMonArray());
-		}
-		return null;
-	}
+//	public Object[] getMonsters(int monKey){
+//		switch(monKey){
+//		case 0:
+//			return extractName(DCXmlRpcServer.DC.getMainRegMonArray());
+//		}
+//		return null;
+//	}
 	
 	private Object[] extractName(ArrayList arrIn) {
 		
 		ArrayList arrOut = new ArrayList();
 		
 		for(int x = 0;x <arrIn.size();x++){
-			arrOut.add(((Monster)arrIn.get(x)).getName());
+			System.out.println(((Monster)arrIn.get(x)).getName());
 		}
 		
 		return arrOut.toArray();
@@ -85,6 +87,198 @@ public class DCXmlRpcWrapper {
 	public String test(int boss){
 		
 		return ((Monster)(DCXmlRpcServer.DC.getMainBossArray().get(boss))).getMonID();
+	}
+	
+		public Object[] refreshMonsterField(int nClass, int nDiff){
+
+
+			ArrayList nMonster = new ArrayList();
+			
+		switch(nClass){
+		case 0:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainRegMonArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getRealName() + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainRegMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getRealName() + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainRegMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getRealName() + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainRegMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff()).equals("H")){		
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getRealName() + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainRegMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		case 1:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainMinMonArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getRealName() + " - " + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainMinMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getRealName() + " - " + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainMinMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getRealName() + " - " + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainMinMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff()).equals("H")){	
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getRealName() + " - " + " (" + (String)((Monster)DCXmlRpcServer.DC.getMainMinMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		case 2:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainChampMonArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainChampMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainChampMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainChampMonArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff()).equals("H")){		
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainChampMonArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		case 3:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainUniqArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff()).equals("H")){		
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		case 4:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainSupUniqArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainSupUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainSupUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainSupUniqArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff()).equals("H")){			
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainSupUniqArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		case 5:
+			switch(nDiff){
+			case 0:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainBossArray().size();x=x+1){
+					nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff() + ")");
+				}
+				break;
+			case 1:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainBossArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff()).equals("N")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 2:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainBossArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff()).equals("NM")){
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			case 3:
+				for(int x = 0; x< DCXmlRpcServer.DC.getMainBossArray().size();x=x+1){
+					if(((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff()).equals("H")){		
+						nMonster.add((String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getRealName()+ " (" + (String)((Monster)DCXmlRpcServer.DC.getMainBossArray().get(x)).getMonDiff() + ")");
+					}
+				}
+				break;
+			}
+			break;
+		}
+
+		return nMonster.toArray();
+		
 	}
 	
 }
