12ec1ae8b993bba071d10e52e4c5ad6a91ca5e04
mmelnik <mmelnik@gmail.com>
2022-04-11 21:13:26 +0300

    refactoring: remove auto-generated comment in D2FileReader class <eom>

--- GoMule/src/main/java/randall/d2files/D2FileReader.java
+++ GoMule/src/main/java/randall/d2files/D2FileReader.java
@@ -27,9 +27,6 @@ import java.io.FileInputStream;
 
 /**
  * @author Marco
- * <p>
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class D2FileReader {
 
